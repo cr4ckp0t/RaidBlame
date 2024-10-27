23ba57ba47f9cfdd7daa7729a70d2046e8c5d46d
Adam Koch <me@adamko.ch>
2024-10-27 10:43:20 -0400

    Update TOC for new patch.

--- RaidBlame.lua
+++ RaidBlame.lua
@@ -7,4 +7,4 @@ local GetNumGroupMembers = _G["GetNumGroupMembers"]
 
 SLASH_RAIDBLAME1 = "/rb"
 SLASH_RAIDBLAME2 = "/raidblame"
-SlashCmdList["RAIDBLAME"] = function(msg) SendChatMessage(("Blame randomly assigned to %s."):format(UnitName(("raid%d"):format(random(GetNumGroupMembers())))), "raid") end
+SlashCmdList["RAIDBLAME"] = function(msg) SendChatMessage(("Blame randomly assigned to %s."):format(UnitName(("raid%d"):format(random(GetNumGroupMembers())))), "INSTANCE_CHAT") end
